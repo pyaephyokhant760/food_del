72416202c35161ff5160cabc6fb0d48173901e28
Pyae Phyo Khant <pyaephyokhant2020dc@gmial.com>
2026-01-19 16:26:47 +0630

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, recusandae harum perferendis praesentium eaque excepturi ullam natus illo aliquam quasi!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+import { useState } from "react"
+
+const Navbar = () => {
+
+    const [menu,setMenu] = useState("home");
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li onClick={() =>setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+        <li onClick={() =>setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
+        <li onClick={() =>setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile App</li>
+        <li onClick={() =>setMenu("contant-us")} className={menu === "contant-us" ? "active" : ""}>Contant Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+        </div>
+        <button>Sing In</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
