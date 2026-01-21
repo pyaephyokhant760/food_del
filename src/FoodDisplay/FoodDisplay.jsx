118a0c8a319c6d1cb982ddbb770a4e02f8d27260
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2026-01-21 21:07:05 +0630

    context

---
+++ src/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import "./FoodDisplay.css"
+import { StoreContext } from '../content/StoreContext'
+
+
+const FoodDisplay = () => {
+
+    const {food_list } = useContext(StoreContext)
+
+  return (
+    <div className='food-display' id='food-display'>
+        <h2>Top dishes near you</h2>
+    </div>
+  )
+}
+
+export default FoodDisplay
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import "./Home.css"
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import { useState } from "react"
+
 const Home = () => {
+  const [category , setcategory] = useState("All");
   return (
     <div>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu category={category} setcategory={setcategory}/>
     </div>
   )
 }
