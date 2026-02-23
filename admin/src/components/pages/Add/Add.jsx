b246c539a9e242bfa93fd84b7a0ee56869d54b36
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2026-02-23 22:55:02 +0630

    admin_add

--- admin/src/components/pages/Add/Add.jsx
+++ admin/src/components/pages/Add/Add.jsx
@@ -1,30 +1,72 @@
 import React from 'react'
 import './Add.css'
+import { assets } from '../../../assets/assets'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const Add = () => {
+    const [image,setImage] = useState(false);
+    const [data,setData] = useState({
+        name:"",
+        description:"",
+        price:"",
+        category:"Salad"
+    })
+    const onChangeHandler = (event)=>{
+       const name = event.target.name;
+       const value = event.target.value;
+       setData(data=>({...data,[name]:value}))
+    }
+
+    const onsubmitHandler = async()=>{
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append("name",data.name);
+        formData.append("description",data.description);
+        formData.append("price",data.price);
+        formData.append("category",data.category);
+        formData.append("image",image);
+        const response = await fetch("http://localhost:5000/api/food/add",{
+            method:"POST",
+            body:formData
+        })
+        const data = await response.json();
+        if(data.success){
+            setData({
+                name:"",
+                description:"",
+                price:"",
+                category:"Salad"
+            })
+            setImage(false)
+        }
+    }
+    useEffect(()=>{
+        console.log(data);
+    },[data])
   return (
     <div className='add'>
-        <form action="" className='flex-col'>
+        <form onSubmit={onsubmitHandler} action="" className='flex-col'>
             <div className="add-img-upload flex-col">
                 <p>Upload Image</p>
                 <label htmlFor="image">
-                    <img src={assets.upload_area} alt="" />
+                    <img src={image?URL.createObjectURL(image):assets.upload_area} alt="" />
                 </label>
-                <input type="file" id='image' hidden required/>
+                <input onClick={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required/>
             </div>
             <div className="add-product-name flex-col">
                 <p>Product Name</p>
-                <input type="text" placeholder='Type here' name='name'/>
+                <input onClick={onChangeHandler} value={data.name} type="text" placeholder='Type here' name='name'/>
             </div>
             <div className="add-product-description flex-col">
                 <p>Product Description</p>
-                <textarea name="description" id="" cols="30" rows="6" placeholder='Write description here'></textarea>
+                <textarea onClick={onChangeHandler} value={data.description} name="description" id="" cols="30" rows="6" placeholder='Write description here'></textarea>
             </div>
             
             <div className="add-category-price">
                 <div className="add-category flex-col">
                     <p>Product Category</p>
-                    <select name="category" id="">
+                    <select onClick={onChangeHandler} value={data.category} name="category" id="">
                         <option value="Salad">Salad</option>
                         <option value="Rolls">Rolls</option>
                         <option value="Pasta">Pasta</option>
@@ -33,9 +75,10 @@ const Add = () => {
                 </div>
                 <div className="add-price flex-col">
                     <p>Product Price</p>
-                    <input type="text" placeholder='Type here' name='price'/>
+                    <input onClick={onChangeHandler} value={data.price} type="text" placeholder='Type here' name='price'/>
                 </div>
             </div>
+            <button type='submit' className='add-btn'>ADD PRODUCT</button>
 
         </form>
     </div>
