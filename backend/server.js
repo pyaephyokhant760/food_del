573d7b3bf9fbb445771f8e7d16acf668e5d749d3
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2026-03-04 21:42:30 +0630

    dokcer_and_admin

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
 
 // app config
 const app = express()
@@ -10,12 +11,15 @@ const port = 4000
 //middleware
 app.use(express.json())
 app.use(cors())
-
+app.get("/",(req,res)=>{
+    res.send("PPK aaaaaa bbbbbb")
+})
 //db connect
 connectDB();
 //api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
 
 app.get("/",(req,res)=>{
     res.send("Hello World")
